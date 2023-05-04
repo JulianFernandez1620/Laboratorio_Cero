46d55cdb199ef5f1b1f9c93d5b3b110d588d9705
JulianFernandez1620 <jufernandezs@unal.edu.co>
2023-05-04 06:27:37 -0500

    primera version frontend

---
+++ frontend/src/elements/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const About = () => (
+    <div className="container m-4">
+        <h1>About</h1>
+    <div  className="container m-4">
+    <h2>Aplicacion web - CRUD</h2>
+    <h3>Ingenieria de Software</h3>
+    <h3>Universidad Nacional de Colombia</h3>
+    </div>
+    </div>
+)
